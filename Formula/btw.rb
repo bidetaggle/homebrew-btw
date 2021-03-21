6dfc2524d67be0d45abfd83c6f455a45c925348d
bidetaggle <bidetaggle@mac>
2021-03-21 16:52:16 +0100

    fill missing properties

--- Formula/btw.rb
+++ Formula/btw.rb
@@ -1,11 +1,11 @@
 class Btw < Formula
   include Language::Python::Virtualenv
 
-  desc ""
-  homepage ""
-  url "https://codeload.github.com/bidetaggle/bytheway/tar.gz/refs/tags/v0.1-alpha"
-  version "0.1.0"
-  sha256 ""
+  desc "NLP command line assistant powered by OpenAI."
+  homepage "https://github.com/bidetaggle/bytheway/tree/dev#by-the-way"
+  url "https://github.com/bidetaggle/bytheway/archive/refs/tags/v0.1-alpha.tar.gz"
+  version "0.1"
+  sha256 "636221c308339af8ca5302f5f5a6241cd84a6448b7da8893f01a183653a06c77"
   license "MIT"
 
   depends_on "python"
