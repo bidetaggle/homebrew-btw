9e73e47b2512986ae537484e943219eb7bd1315a
bidetaggle <bidetaggle@mac>
2021-03-22 17:09:40 +0100

    test

--- Formula/btw.rb
+++ Formula/btw.rb
@@ -1,11 +1,8 @@
 class Btw < Formula
-  include Language::Python::Virtualenv
-
   desc "NLP command-line assistant powered by OpenAI"
-  homepage "https://github.com/bidetaggle/bytheway/tree/dev#by-the-way"
-  url "https://github.com/bidetaggle/bytheway/archive/refs/tags/v0.1-alpha.tar.gz"
-  version "0.1"
-  sha256 "636221c308339af8ca5302f5f5a6241cd84a6448b7da8893f01a183653a06c77"
+  homepage "https://github.com/bidetaggle/btw"
+  url "https://github.com/bidetaggle/btw/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "e04661573e962bfc5d4ac29b92adc9c0f3a9317b758ed96c2f59f0db187804f2"
   license "MIT"
 
   depends_on "python"
@@ -13,7 +10,7 @@ class Btw < Formula
   def install
     virtualenv_install_with_resources
   end
-
+  
   test do
     system "false"
   end
