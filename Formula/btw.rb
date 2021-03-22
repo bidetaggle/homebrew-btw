a04bc6be829dc7e3fc4bf6fa63d1987c790f9986
bidetaggle <bidetaggle@mac>
2021-03-22 17:22:07 +0100

    add python version

--- Formula/btw.rb
+++ Formula/btw.rb
@@ -5,7 +5,7 @@ class Btw < Formula
   sha256 "e04661573e962bfc5d4ac29b92adc9c0f3a9317b758ed96c2f59f0db187804f2"
   license "MIT"
 
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     virtualenv_install_with_resources
